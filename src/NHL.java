1f3725ab66eaa4537c4ee625e0b0de9448a4db9e
Gudrun Vene <gudrun.vene@khk.ee>
2019-09-30 15:42:23 +0300

    nhl algus

---
+++ src/NHL.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import nhlstats.NHLStatistics;
+
+public class NHL {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Top ten by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.top(10);
+    }
+}
