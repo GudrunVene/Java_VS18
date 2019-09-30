183b3699c79ef80db1eee33766824c360c35c724
Gudrun Vene <gudrun.vene@khk.ee>
2019-09-30 14:53:47 +0300

    multiplication

---
+++ src/Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int firstNumber = 277;
+        int secondNumber = 111;
+        int multiplicationNumber = firstNumber * secondNumber;
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiplicationNumber);
+    }
+}
