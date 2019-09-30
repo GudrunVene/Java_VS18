42229cae8f56fd9a5ac65e04f45d71a0e4613fea
Gudrun Vene <gudrun.vene@khk.ee>
2019-09-30 14:52:11 +0300

    addition

---
+++ src/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int firstNumber = 73457;
+        int secondNumber = 12888;
+        int sum = firstNumber + secondNumber;
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+    }
+}
