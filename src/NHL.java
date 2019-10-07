93287926155a99971911ec63e31a8a9d8e3d9ffa
Gudrun Vene <gudrun.vene@khk.ee>
2019-10-07 10:35:59 +0300

    NHL statistics, part 1

--- src/NHL.java
+++ src/NHL.java
@@ -8,5 +8,21 @@ public class NHL {
         System.out.println("Top ten by points");
         NHLStatistics.sortByPoints();
         NHLStatistics.top(10);
+
+        System.out.println("Top 25 players based on penalty amounts");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+
+        System.out.println("Statistics for Sidney Crosby");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+
+        System.out.println("Statistics for Philadelphia Flyers");
+        NHLStatistics.teamStatistics("PHI");
+        /*Statistic seem to be in order of penalty amount. Maybe because i last sorted statistics by penalties.*/
+
+        System.out.println("players in Anaheim Ducks ordered by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
+
     }
 }
