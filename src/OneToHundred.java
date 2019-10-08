69620e0e1eb6927d2f66b14deab5fd1bb7e45b0e
Gudrun Vene <gudrun.vene@khk.ee>
2019-10-08 21:19:25 +0300

    Exercise 27: From one to a hundred

---
+++ src/OneToHundred.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class OneToHundred {
+
+    public static void main(String [] args){
+
+        Scanner reader = new Scanner(System.in);
+        int number = 1;
+
+
+        while (number < 101){
+            System.out.println(number);
+            number ++;
+        }
+    }
+}
