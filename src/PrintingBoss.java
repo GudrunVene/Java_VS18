88ecb85e9ab331b7922c3ddf38a46ae0d7daa0ed
Gudrun Vene <gudrun.vene@khk.ee>
2019-10-14 15:16:54 +0300

    Exercise 40.3: Printing a Christmas tree

--- src/PrintingBoss.java
+++ src/PrintingBoss.java
@@ -8,7 +8,7 @@ public class PrintingBoss {
                 System.out.print("*");
                 i++;
             }
-            System.out.println(" ");
+
 
     }
 
@@ -32,9 +32,37 @@ public class PrintingBoss {
 
     }
 
+    private static void xmasTreeLeg(int height){
+        int i = 0;
+        while (i < 2) {
+            printWhitespaces(height );
+            printStars(3);
+            printWhitespaces(height );
+            System.out.println(" ");
+            i ++;
+        }
+
+    }
+
+    private static void xmasTree(int height){
+
+        int i = 1;
+        while (i <= height) {
+            printWhitespaces(height +2 -i );
+            printStars(i);
+            printStars(i-1);
+            printWhitespaces(height +2 -i);
+            System.out.println(" ");
+            i ++;
+        }
+
+        xmasTreeLeg(height);
+    }
+
+
     public static void main(String[] args) {
 
-        printTriangle(4);
+        xmasTree(4);
 
     }
 }
