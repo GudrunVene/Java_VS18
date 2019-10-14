f99a4d3787ec0159762c7fed58da6c465c82b96a
Gudrun Vene <gudrun.vene@khk.ee>
2019-10-14 12:26:53 +0300

    Exercise 39.4: Printing a left-aligned triangle

--- src/LeftTriangle.java
+++ src/LeftTriangle.java
@@ -1,2 +1,30 @@
 public class LeftTriangle {
+
+    private static void printStars(int amount) {
+
+        int i = 1;
+        while (i <= amount){
+            System.out.print("*");
+            i ++;
+        }
+        System.out.println(" ");
+
+    }
+
+    private static void  printTriangle(int size){
+
+        int i = 1;
+        while (i <= size) {
+            printStars(i);
+            i ++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(4);
+
+
+    }
 }
