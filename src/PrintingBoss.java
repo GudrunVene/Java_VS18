fc6e35564ae921a68ffb6120be769cdddae74b11
Gudrun Vene <gudrun.vene@khk.ee>
2019-10-14 14:34:01 +0300

    Exercise 40.2: Printing a right-aligned triangle

--- src/PrintingBoss.java
+++ src/PrintingBoss.java
@@ -1,18 +1,40 @@
 public class PrintingBoss {
 
-    private static void printWhitespaces(int size) {
+    private static void printStars(int amount) {
+
+            int i = 1;
+
+            while (i <= amount) {
+                System.out.print("*");
+                i++;
+            }
+            System.out.println(" ");
+
+    }
+
+    private static void printWhitespaces(int amount) {
+
+        while ( amount >= 0 ){
+            System.out.print(" ");
+            amount--;
+        }
+    }
+
+    private static void  printTriangle(int size){
 
         int i = 1;
-        while (i <= size){
-            System.out.print(" ");
+        while (i <= size) {
+            printWhitespaces(size - i);
+            printStars(i);
+
             i ++;
         }
+
     }
 
     public static void main(String[] args) {
 
-        printWhitespaces(4);
-
+        printTriangle(4);
 
     }
 }
