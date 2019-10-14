d62e0e5b95ee075c6560feaa2b8cf015907c9821
Gudrun Vene <gudrun.vene@khk.ee>
2019-10-14 12:30:34 +0300

    Exercise 40.1: Printing stars and whitespaces

---
+++ src/PrintingBoss.java
@@ -0,0 +1,18 @@
+public class PrintingBoss {
+
+    private static void printWhitespaces(int size) {
+
+        int i = 1;
+        while (i <= size){
+            System.out.print(" ");
+            i ++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printWhitespaces(4);
+
+
+    }
+}
