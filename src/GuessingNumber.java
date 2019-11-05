c8eb4b3f549abec6c77f0c7839eb0c0aa5925d3f
Gudrun Vene <gudrun.vene@khk.ee>
2019-11-05 11:41:40 +0200

    41  Guessing game

--- src/GuessingNumber.java
+++ src/GuessingNumber.java
@@ -1,19 +1,45 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessingNumber {
 
     public static void main(String args[]) {
 
+        int numberForGuessing = drawNumber();
 
-        drawNumber(101);
+        Scanner reader = new Scanner(System.in);
+
+        int i = 0;
+        boolean win = false;
+
+        while (win == false) {
+
+            System.out.print("Guess a number: ");
+            int guessedNumber = reader.nextInt();
+
+            if (guessedNumber > 100 | guessedNumber < 0) {
+                System.out.println("Guess a number between 0 and 100");
+            }
+            else if (guessedNumber > numberForGuessing) {
+                i++;
+                System.out.println("The number is lesser, guesses made: " + i);
+            } else if (guessedNumber < numberForGuessing) {
+                i++;
+                System.out.println("The number is greater, guesses made: " + i);
+            } else if (guessedNumber == numberForGuessing) {
+                win = true;
+            }
+        }
+        System.out.println("Congratulations, your guess is correct!");
 
     }
 
-    private static int drawNumber(int max){
+    private static int drawNumber(){
 
         Random random = new Random();
-        return random.nextInt(max);
-
+        int numberToGuess = random.nextInt(100);
+        /*System.out.println(numberToGuess);*/
+        return numberToGuess;
     }
 
 }
