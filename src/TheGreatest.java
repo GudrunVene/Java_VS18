3d059494fd2f18ceca3510c15c7ed143eada97af
Gudrun Vene <gudrun.vene@khk.ee>
2019-11-06 12:19:01 +0200

    Exercise 66: The Greatest

---
+++ src/TheGreatest.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TheGreatest {
+
+    public static int greatest(ArrayList<Integer> list) {
+
+        Collections.sort(list);
+        int number = list.get(list.size()-1);
+        return number;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+
+        System.out.println("The greatest number is: " + greatest(list));
+    }
+}
