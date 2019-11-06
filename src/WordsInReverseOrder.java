c75deec511666c34e6021a5a36cbdd57cc2d13d9
Gudrun Vene <gudrun.vene@khk.ee>
2019-11-06 11:03:28 +0200

    Exercise 59: Words in reverse order

---
+++ src/WordsInReverseOrder.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class WordsInReverseOrder {
+
+    public static void main(String[] args){
+
+        ArrayList<String> words = new ArrayList<String>();
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type a word: ");
+        String enteredWord = reader.nextLine();
+
+        while ( !enteredWord.isEmpty()){
+            words.add(enteredWord);
+            System.out.print("Type a word: ");
+            enteredWord = reader.nextLine();
+
+        }  if ( enteredWord.isEmpty() ) {
+            Collections.sort(words);
+            Collections.reverse(words);
+            System.out.println("You typed the following words: ");
+            for (String word : words) {
+                System.out.println( word );
+            }
+        }
+    }
+}
