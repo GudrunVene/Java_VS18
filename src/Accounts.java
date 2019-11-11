45ed0e6e8e95b91bb44dc0067ebf5c5a499046c9
Gudrun Vene <gudrun.vene@khk.ee>
2019-11-11 18:32:28 +0200

    Exercise 72.2: Your first money transfer

--- src/Accounts.java
+++ src/Accounts.java
@@ -5,7 +5,7 @@ public class Accounts {
         Account bartosAccount = new Account("Barto's account",100.00);
         Account bartosSwissAccount = new Account("Barto's account in Switzerland",1000000.00);
 
-        System.out.println("Initial state");
+       /* System.out.println("Initial state");
         System.out.println(bartosAccount);
         System.out.println(bartosSwissAccount);
 
@@ -16,18 +16,23 @@ public class Accounts {
 
         System.out.println("Final state");
         System.out.println(bartosAccount);
-        System.out.println(bartosSwissAccount);
+        System.out.println(bartosSwissAccount);*/
 
-        newAccount("Gudrun");
+        Account mattsAccount = newAccount("Matt", 1000);
+        Account myAccount = newAccount("My", 0);
+
+        mattsAccount.withdrawal(100);
+        System.out.println("Matt's account balance is " + mattsAccount);
+        myAccount.deposit(100);
+        System.out.println("My account balance is " + myAccount);
     }
 
-    public static void newAccount(String owner) {
+    public static Account newAccount(String owner, double balance) {
 
-        Account ownerAccount = new Account(owner, 100 );
-        System.out.println("Initial state");
+        Account ownerAccount = new Account(owner, balance );
+        System.out.println("Initial state of new account");
         System.out.println(ownerAccount);
-        ownerAccount.deposit(20);
-        System.out.println("Final state");
-        System.out.println(ownerAccount);
+        return ownerAccount;
+
     }
 }
