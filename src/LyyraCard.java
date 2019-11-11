331e203b4ca5810e96a740a15216cd1615cc1261
Gudrun Vene <gudrun.vene@khk.ee>
2019-11-11 21:30:20 +0200

    Exercise 77.1: Class skeleton

---
+++ src/LyyraCard.java
@@ -0,0 +1,14 @@
+public class LyyraCard {
+
+    private double balance;
+
+    public LyyraCard(double balanceAtStart) {
+        // write code here
+        this.balance = balanceAtStart;
+    }
+
+    public String toString() {
+        // write code here
+        return "The card has " + this.balance + " euros";
+    }
+}
