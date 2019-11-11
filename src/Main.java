af0bf62a5209bd2b7529f02bca49f40ad42bb393
Gudrun Vene <gudrun.vene@khk.ee>
2019-11-11 21:22:59 +0200

    Person

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Menu food = new Menu("Hamburger");
+        Person bob = new Person("Bob");
+        Person andy = new Person("Andy");
 
-        food.addMeal("Fish'n'Chips");
-        food.addMeal("Sauerkraut");
+        int i = 0;
+        while ( i < 30 ) {
+            bob.becomeOlder();
+            i++;
+        }
 
-        food.printMeals();
-        food.clearMenu();
-        food.printMeals();
+        andy.becomeOlder();
 
+        System.out.println( andy ); // same as System.out.println( andy.toString() );
+        System.out.println( bob ); // same as System.out.println( bob.toString() );
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
+
+    public void printPerson() {
+        System.out.println(this.name + ", age " + this.age + " years");
+    }
+
+    public void becomeOlder() {
+        this.age = this.age + 1;
+    }
+
+    public boolean isAdult(){
+        return this.age >= 18;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        return this.name + ", age " + this.age + " years";
+    }
+
+}
