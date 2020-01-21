ebabf5109b66f5525073841b381a0b064d8889da
Gudrun Vene <gudrun.vene@khk.ee>
2020-01-22 01:08:23 +0200

    Exercise 79.4: Many sums

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        NumberStatistics stats = new NumberStatistics();
+        NumberStatistics statsAll = new NumberStatistics();
+        NumberStatistics statsEven = new NumberStatistics();
+        NumberStatistics statsOdd = new NumberStatistics();
 
         while (true) {
             System.out.print("Type numbers: ");
             int number = Integer.parseInt(reader.nextLine());
             if (number != -1) {
-                stats.addNumber(number);
+                statsAll.addNumber(number);
+                if (number % 2 == 0){
+                    statsEven.addNumber(number);
+                } else if (number % 2 != 0){
+                    statsOdd.addNumber(number);
+                }
             } else if (number == -1) {
-                System.out.println("Amount: " + stats.amountOfNumbers());
-                System.out.println("sum: " + stats.sum());
-                System.out.println("average: " + stats.average());
+                System.out.println("sum: " + statsAll.sum());
+                System.out.println("sum of even : " + statsEven.sum());
+                System.out.println("sum of odd: " + statsOdd.sum());
                 break;
 
             }
