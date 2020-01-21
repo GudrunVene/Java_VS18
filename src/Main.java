7d7276e8916ead4e863b6666b9ea24cb6258abe0
Gudrun Vene <gudrun.vene@khk.ee>
2020-01-22 01:01:19 +0200

    Exercise 79.3: Asking for numbers from the user

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
-        System.out.println("Amount: " + stats.amountOfNumbers());
-        System.out.println("sum: " + stats.sum());
-        System.out.println("average: " + stats.average());
+
+        while (true) {
+            System.out.print("Type numbers: ");
+            int number = Integer.parseInt(reader.nextLine());
+            if (number != -1) {
+                stats.addNumber(number);
+            } else if (number == -1) {
+                System.out.println("Amount: " + stats.amountOfNumbers());
+                System.out.println("sum: " + stats.sum());
+                System.out.println("average: " + stats.average());
+                break;
+
+            }
+        }
+
+
     }
 }
