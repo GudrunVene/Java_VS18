b7f671b82a50cafa88fb90318ba7e24e397bdc87
Gudrun Vene <gudrun.vene@khk.ee>
2020-01-22 02:07:11 +0200

    Exercise 80: Rolling the dice

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        NumberStatistics statsAll = new NumberStatistics();
-        NumberStatistics statsEven = new NumberStatistics();
-        NumberStatistics statsOdd = new NumberStatistics();
+        Dice dice = new Dice(6);
 
-        while (true) {
-            System.out.print("Type numbers: ");
-            int number = Integer.parseInt(reader.nextLine());
-            if (number != -1) {
-                statsAll.addNumber(number);
-                if (number % 2 == 0){
-                    statsEven.addNumber(number);
-                } else if (number % 2 != 0){
-                    statsOdd.addNumber(number);
-                }
-            } else if (number == -1) {
-                System.out.println("sum: " + statsAll.sum());
-                System.out.println("sum of even : " + statsEven.sum());
-                System.out.println("sum of odd: " + statsOdd.sum());
-                break;
-
-            }
+        int i = 0;
+        while ( i < 10 ) {
+            System.out.println( dice.roll() );
+            i++;
         }
-
 
     }
 }
